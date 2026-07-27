82c335e6b1529c91157bcba3f7b75c344e990102
hasanazeezz <hasanazeez@hotmail.com>
2026-07-28 02:03:16 +0400

    Add files via upload

---
+++ fibonacci_generator.py
@@ -0,0 +1,12 @@
+# Fibonacci Generator For My Internship At Rhombix Technologies
+n = int(input("Enter the number of terms: "))
+a, b = 0, 1
+
+if n <= 0:
+    print("Please enter a positive number.")
+else:
+    print("Fibonacci Series:")
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+        
